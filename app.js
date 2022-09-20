f706ac37a1819f37ac9ee2c6b0e1b2412dd6061c
Haley Wakamatsu <japanyoshi777@gmail.com>
2022-09-20 15:24:37 +0900

    Lots more logging.

--- app.js
+++ app.js
@@ -103,34 +103,34 @@ wss.on('connection', function(ws) {
                 break;
             case 'hello':
                 // msg.name has last used name
-                console.log(Object.keys(sockets), data.name, Object.keys(sockets).includes(data.name));
+                console.log("Socket " + data.name + " says hello.");
                 if (Object.keys(sockets).includes(data.name)) {
                     // name already used
                     sendError("Requested ID is already in use. Please log in only once per browser.");
                     ws.close();
-                    console.log("duplicate ID");
+                    console.error("Closed socket for duplicate ID.");
                     return;
                 // sure, implicit type casting is the js way but i want it to be explicit because i am a messy coder
                 } else if (Boolean(data.name) == false) {
-                    console.log(`assigning new ID: ${name}`);
+                    console.log(`Assigning new ID: ${name}`);
                 } else {
                     name = data.name;
-                    console.log(`assigning requested ID: ${data.name}`);
+                    console.log(`Assigning requested ID: ${data.name}`);
                 }
                 ws.send(JSON.stringify({
                     type: 'onGetMyName',
                     name: name
                 }));
-                console.log("sent onGetMyName (after hello)");
+                console.log("Sent name to socket (after hello).");
                 sockets[name] = ws;
-                console.log("sockets dict now has these keys: " + Object.keys(sockets))
+                console.log("Current connections: " + Object.keys(sockets).length + " " + Object.keys(sockets));
                 break;
             case 'getMyName':
                 ws.send(JSON.stringify({
                     type: 'onGetMyName',
                     name: name
                 }));
-                console.log("sent onGetMyName (after getMyName)");
+                console.log("Sent name " + name + " to socket.");
                 break;
             case 'broadcast':
                 Object.keys(sockets).forEach(key => {
@@ -143,59 +143,66 @@ wss.on('connection', function(ws) {
                         s.send(JSON.stringify(data));
                         console.log("sent onBroadcast to " + key);
                     } else {
-                        console.log("could not send because sockets[" + key + "] is " + sockets[key]);
+                        console.error("Could not send because sockets[" + key + "] is " + sockets[key]);
                     }
                     sendOk(`Message broadcast to all clients.`);
                 });
                 break;
             case 'message':
-                console.log("received message")
+                console.log("Received message from " + name + ".");
                 if (sockets[data.to] != undefined) {
                     data.type = 'onMessage';
                     sockets[data.to].send(JSON.stringify(data));
                     sendOk(`Message sent to client ${data.to}.`);
-                    console.log("sent message (OK)");
+                    console.log("Sent message to " + data.to + ". (OK)");
                 } else {
                     sendError(`Could not find client with name ${data.to}.`);
-                    console.log("sent message (client not found error). sockets dict has these keys:", Object.keys(sockets));
+                    console.error("Could not find recipient " + data.to + ". Sent back an error.");
                 }
                 break;
             case 'hostRoom':
-                console.log("received hostRoom");
+                console.log("Received hostRoom from " + name + ".");
                 // expected keys: data.roomCode, data.gameName
-                if ("roomCode" in data && "gameName" in data) {
+                if (
+                    data.hasOwnProperty("roomCode")
+                    && data.hasOwnProperty("gameName")
+                    && data.hasOwnProperty("controller")
+                    && data.hasOwnProperty("maxPlayers")
+                    && data.hasOwnProperty("maxAudience")
+                ) {
                     // OK
                     if (data.roomCode.length != 4) {
                         sendError("Room code must be 4 characters long.");
+                        console.error("Invalid room code length: " + data.roomCode);
                         break;
                     }
                     if (isBadRoomCode(data.roomCode)) {
                         sendError("Room code is forbidden.");
+                        console.error("Forbidden room code: " + data.roomCode);
                         break;
                     }
                     if (data.gameName.length == 0) {
                         sendError("Game name must not be empty.");
+                        console.error("Empty game name.");
                         break;
-                    }
-                    if (!data.hasOwnProperty('maxPlayers')) {
-                        sendError("Missing property maxPlayers (integer).");
-                    }
-                    if (!data.hasOwnProperty('maxAudience')) {
-                        sendError("Missing property maxAudience (integer).");
                     }
                     let found = false
                     for (const [k, v] of Object.entries(rooms)) {
                         if (v.host == name) {
                             // you're already hosting a different room
                             sendError("You are already hosting a room with the room code " + k);
+                            console.error(name + " is already hosting a room: " + k + ".");
                             found = true;
                             break;
                         }
                     }
-                    if (found) break;
+                    if (found) {
+                        break;
+                    }
                     if (rooms[data.roomCode]) {
                         // a room with that room code exists
                         sendError("The room code " + data.roomCode + " is already taken.");
+                        console.error("The room code " + data.roomCode + " is already taken.");
                         break;
                     }
                     rooms[data.roomCode] = {
@@ -214,22 +221,26 @@ wss.on('connection', function(ws) {
                         type: 'onRoomMade',
                         roomCode: data.roomCode
                     }));
+                    console.log("Checks succeeded. Room is created.");
                 } else {
                     // Not enough data
-                    sendError("hostRoom message must contain the following keys: roomCode (string length 4), gameName (non-empty string)");
+                    sendError("hostRoom message must contain the following keys: roomCode (string length 4), gameName (non-empty string), controller (HTML file name), maxPlayers (int >= 1), maxAudience (int >= 0).");
+                    console.error("Missing properties. Room not created.")
                 }
                 break;
             case 'queryRoom':
-                console.log("received queryRoom");
+                console.log("Received queryRoom from " + name + ".");
                 // expected keys: data.roomCode, data.nick (may be empty)
-                if ("roomCode" in data) {
+                if (data.hasOwnProperty("roomCode")) {
                     // OK
                     if (data.roomCode.length != 4) {
                         sendError("Room code must be 4 characters long.");
+                        console.error("Invalid room code: " + data.roomCode + ".");
                         break;
                     }
                     if (isBadRoomCode(data.roomCode)) {
                         sendError("This room code is forbidden.");
+                        console.error("Forbidden room code: " + data.roomCode + ".");
                         break;
                     }
                     if (rooms[data.roomCode]) {
@@ -239,23 +250,26 @@ wss.on('connection', function(ws) {
                             gameName: rooms[data.roomCode].gameName,
                             status: rooms[data.roomCode].status
                         }));
-                        console.log("room exists, sent roomFound message");
+                        console.log("Room " + data.roomCode + " exists. Sent roomFound message to " + name + ".");
                     } else {
                         // room doesn't exist
                         ws.send(JSON.stringify({
                             type: 'roomNotFound'
                         }));
-                        console.log("room doesn't exist, sent roomNotFound message");
+                        console.log("Room doesn't exist. Sent roomNotFound message to " + name + ".");
                     }
+                } else {
+                    console.error("The message does not contain a room code.");
                 }
                 break;
             case 'joinRoom':
-                console.log("received joinRoom");
+                console.log("Received joinRoom from " + name + ".");
                 // expected keys: data.roomCode, data.nick (may be empty)
-                if ("roomCode" in data && "nick" in data) {
+                if (data.hasOwnProperty("roomCode") && data.hasOwnProperty("nick")) {
                     // OK
                     if (data.roomCode.length != 4) {
                         sendError("Room code must be 4 characters long.");
+                        console.error("Invalid room code: " + data.roomCode + ".");
                         return;
                     }
                     if (rooms[data.roomCode]) {
@@ -275,12 +289,13 @@ wss.on('connection', function(ws) {
                                 message: "Rejoined Room " + data.roomCode + " successfully.",
                                 controller: rm.controller
                             }) );
-                            console.log("Welcome back!");
+                            console.log("Welcome back to Room " + data.roomCode + ", " + name + "!");
                             break;
                         }
                         // is room full?
                         else if (rm.status == ROOM_STAT.FULL_AUDI) {
                             sendError("Room " + data.roomCode + " is full.");
+                            console.log("Declined join; specified room is full.");
                             break;
                         }
                         // is game in progress?
@@ -291,6 +306,7 @@ wss.on('connection', function(ws) {
                         // is game ended?
                         else if (rm.status == ROOM_STAT.INGAME) {
                             sendError("Room " + data.roomCode + " has already finished playing.");
+                            console.log("Declined join; specified game is already over.");
                             break;
                         }
                         // all clear, join.
@@ -311,7 +327,7 @@ wss.on('connection', function(ws) {
                                 message: "Joined Room " + data.roomCode + " successfully.",
                                 controller: rm.controller
                             }) );
-                            console.log("Welcome!");
+                            console.log("Welcome to " + data.roomCode + ", " + name + "!");
                             // keep count of players
                             if (asAudience) {
                                 rm.audience ++;
@@ -333,38 +349,46 @@ wss.on('connection', function(ws) {
                     } else {
                         // room does not exist
                         sendError("Could not find a room with that room code (" + data.roomCode + ").");
+                        console.log("Room not found: " + data.roomCode);
                     }
                 } else {
                     // Not enough data
                     sendError("joinRoom message must contain the following keys: roomCode (string length 4), nick (string length 0 to 12).");
+                    console.error("Missing properties.");
                 }
                 break;
             case 'editRoom':
-                console.log('received editRoom');
+                console.log('received editRoom from ' + name);
                 // expected keys: roomCode, one of gameName, status
                 if (rooms[data.roomCode]) {
                     // room exists
                     if (rooms[data.roomCode].host != name) {
                         sendError("You are not the host of room " + data.roomCode + ".");
+                        console.error(name + " tried to edit a room of which they are not the host.");
                     } else {
                         if ("gameName" in data) {
                             rooms[data.roomCode].gameName = data.gameName;
+                            console.log("Changed gameName to " + data.gameName);
                         } else if ("status" in data) {
                             if (data.status in ROOM_STAT) {
                                 rooms[data.roomCode].status = ROOM_STAT[data.status];
+                                console.log("Changed status to " + data.status + " = " + ROOM_STAT[data.status]);
                             } else {
                                 sendError("Invalid room status code. Valid ones are:\n" + JSON.stringify(ROOM_STAT, null, 2));
+                                console.error("Tried to set the room status code to an invalid value: " + data.status);
                             }
                         } else {
                             sendError("No valid keys to be edited. Supports editing gameName, status");
+                            console.error("No valid keys: gameName, status");
                         }
                     }
                 } else {
                     sendError("No such room exists: " + data.roomCode + ".");
+                    console.error("Room was not found: " + data.roomCode);
                 }
                 break;
             case 'kickFromRoom':
-                console.log('received kickFromRoom');
+                console.log('received kickFromRoom from ' + name);
                 // expected keys: roomCode, name, message (optional)
                 if (rooms[data.roomCode]) {
                     // room exists
@@ -417,7 +441,7 @@ wss.on('connection', function(ws) {
                 }
                 break;
             case 'sendToHost':
-                console.log("received sendToHost: roomCode=" + data.roomCode + " and there are " + Object.keys(rooms).length + " rooms");
+                console.log("received sendToHost from " + name + ": roomCode=" + data.roomCode + " and there are " + Object.keys(rooms).length + " rooms");
                 // expected keys: roomCode (string length 4). should contain additional data
                 if (rooms[data.roomCode]) {
                     // room exists
@@ -433,8 +457,9 @@ wss.on('connection', function(ws) {
                 }
                 break;
             case 'closeRoom':
-                console.log("received closeRoom");
+                console.log("Received closeRoom from " + name);
                 closeRoomBy(name);
+                console.log("Finished closing the room.");
                 break;
             default:
                 console.log("Unrecognized type: ", data.type);
@@ -443,6 +468,7 @@ wss.on('connection', function(ws) {
 
     // When a socket closes, or disconnects, remove it from the array.
     ws.on('close', function() {
+        console.log("Socket closed: " + name);
         // if (closeRoomBy(name)) {
             // don't automatically leave, player may rejoin
             // let found = false
@@ -463,7 +489,7 @@ wss.on('connection', function(ws) {
         // };
         delete sockets[name];
         console.log(`Socket ${name} is disconnected.`);
-        console.log("sockets dict now has these keys: " + Object.keys(sockets))
+        console.log("Sockets dict now has these keys: " + Object.keys(sockets))
         return;
     });
 
